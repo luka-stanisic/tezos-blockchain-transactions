bafefb2c86c89e796b868e54a48d59a7cd745da8
Luka Stanisic <luka.stanisic@aol.com>
2020-07-14 23:08:33 +0200

    Created couple of actions, updated some dep.

--- src/app/root-store/transactions-store/transaction.actions.ts
+++ src/app/root-store/transactions-store/transaction.actions.ts
@@ -1,9 +1,9 @@
 import { createAction, props } from '@ngrx/store';
-import { Update } from '@ngrx/entity';
-
 import { Transaction } from '../../shared/models/transaction;
 
-export const loadTransactions = createAction('[Transactions Page] Load Transactions Request');
+export const loadLatestTransactions = createAction('[Transactions Page] Load Latest Transactions Request');
+export const loadOlderTransactions = createAction('[Transactions Page] Load Latest Transactions Request', props<{ lastRowId: number }>);
+
 export const loadTransactionsSuccess = createAction(
 	'[Transactions API] Load Transactions Success',
 	props<{ transactions: Transaction[] }>()
